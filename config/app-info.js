3824bb446864668837d5224767d3508f67e2c692
khabzox <abdelkabir.ouadoukou@gmail.com>
2024-08-09 13:45:01 +0100

    fix: deploy

--- config/app-info.js
+++ config/app-info.js
@@ -3,7 +3,7 @@ const Appconfig = {
 }
 
 if (process.env.NODE_ENV !== "development") {
-    Appconfig.domainName = "https://4l.xyz";
+    Appconfig.domainName = "https://4links.vercel.app";
 } else {
     Appconfig.domainName = "http://localhost:3001";
 }
